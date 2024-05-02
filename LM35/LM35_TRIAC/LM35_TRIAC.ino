d98ff04fb7723ecd85907eee3f7ded5a70ab12ab
Yohan-Rodriguez <ymrodriguezg@upn.edu.co>
2024-05-02 11:00:32 -0500

    Ajuste de la programación para usar el OpAm LM35

--- LM35/LM35_TRIAC/LM35_TRIAC.ino
+++ LM35/LM35_TRIAC/LM35_TRIAC.ino
@@ -1,40 +1,25 @@
-/* 
-  DHT11
-*/
-#include <DHT_U.h>
-
-// Pin digital conectado al sensor DHT11
-#define DHTPIN A0    
-
-#define DHTTYPE DHT11
-
-DHT dht(DHTPIN, DHTTYPE);
-
 /*
   Controlador del tiempo
 */
 // Util para realizar el temporizador que testeara cada 100 us el contador 
-#include <TimerOne.h>     
+#include <TimerOne.h>
 
 
 /* 
   Declaración de variables para el control de la temperatura
 */
-int temperature = 0; 
+float lectura_lm35;
+// Temperatura control:
+float temp_lm35;
+// Temperatura control:
+float temp_deseada;
+// Temperatura mínima:
+float tem_min;
+// Temperatura media:
+float tem_med;
+// Temperatura máxima:
+float tem_max;
 
-// Temperatura mínima
-int tem_min = 18;
-
-// Rango de temperaturas medias
-int tem_med_1 = 22;
-int tem_med_2 = 24;
-
-// Temperatura máxima
-int tem_max_1 = 25;
-
-// Temperatura para apagar por completo el bombillo
-int tem_max_2 = 27;
-         
 
 /* 
   Declaración de variables para el control del TRIAC
@@ -80,11 +65,9 @@ void setup() {
   // inicializar comunicación Serail a 9600 budios
   Serial.begin(9600);
   
-  // Inicializar la libreria del sensor DHT11
-  dht.begin();
-  Serial.println("Welcome to Microdigisoftn");
+  Serial.println("Bienvenido al controlador de temperatura");
   delay(500);
-  Serial.println("DHT11 Humidity & temperature Sensor");  
+  Serial.println("LM35: Control de temperatura analogo (OpAm)");  
   
   // Esperar un segundo para empezar a acceder al sensor DHT11
   delay(1000);
@@ -125,50 +108,53 @@ void Dimer() {
   Función para sensar la temperature y humedad del sensor DHT11
 */
 void Calc_temperature() {
-  temperature = dht.readTemperature();
-  // Serial.print("Humidity = ");
-  // Serial.print(dht.readHumidity());
-  // Serial.println("%  ");
-  // Serial.print("Temperature = ");
-  Serial.println(temperature);
-  // Serial.println("C  ");
-  // Serial.println("");  
+  lectura_lm35 = analogRead(A1);
+  // Escalar la lectura del potenciometro a valores de voltaje 
+  float volts_lm35 = (lectura_lm35 * 5) / 1023.0;
+  
+  // Obtener temperatuRa en función del valor leido del potenciometro 
+  temp_lm35 = volts_lm35 * 100;
+
+  Serial.println(temp_lm35);
 }
 
+void loop() {
+  // Lectura análoga del valor del potenciometro (ADC) 
+  int read_pot = analogRead(A0) ;
 
+  // Escalar la lectura del potenciometro a valores de voltaje 
+  float volts = (read_pot * 5) / 1023.0;
+  
+  // Obtener temperatuRa en función del valor leido del potenciometro 
+  temp_deseada = volts * 10;
+  Serial.print("Temperatura de control: ");
+  Serial.println(temp_deseada);
 
-void loop () {
+  // Rango de operación del control de temperatura
+  tem_min = temp_deseada - (temp_deseada * 0.1);
+  tem_med = temp_deseada;
+  tem_max = temp_deseada + (temp_deseada * 0.1);
 
-  // Sensar el valor de temperatura y humedad del DHT11
+  // Lectura del valor de temperatura por medio del "LM35"
   Calc_temperature();
 
-  // temperatura menor a 27 °C
-  if (temperature<tem_min) {
+  if (temp_lm35 < tem_min) {
     dim = 21;
   }
   
   // temperatura entre a 27 °C y 30.1 °C 
-  else if (temperature>=tem_min && temperature<tem_med_1) {
+  else if ((temp_lm35 >= tem_min) && (temp_lm35 < tem_med)) {
     dim = 35;
   }
 
-  // temperatura entre 30.1 °C y 30.3 °C 
-  else if (temperature>=tem_med_1 && temperature<tem_med_2) {
-    dim = 45;
-  }
-
-  // temperatura entre 30.3 °C y 30.5 °C 
-  else if (temperature>=tem_med_2 && temperature<tem_max_1) {
-    dim = 60;
-  }
-
-  // temperatura entre 30.5 °C y 30.7 °C 
-  else if (temperature>=tem_max_1 && temperature<tem_max_2) {
-    dim = 68;
+  // temperatura entre a 27 °C y 30.1 °C 
+  else if ((temp_lm35 >= tem_med) && (temp_lm35 < tem_max)) {
+    dim = 35;
   }
 
   // temperatura mayor a 30.7 °C 
-  else if (temperature>=tem_max_2){
+  else if (temp_lm35 >= tem_max){
     dim = 83;
   }
+  delay(300);
 }
